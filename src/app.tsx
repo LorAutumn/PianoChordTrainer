3fb497d880c34e744784348a304e6797dc589a92
LorAutumn <lorenz.herbst@gmx.net>
2023-08-01 12:00:38 +0200

    add keyboard component

--- src/app.tsx
+++ src/app.tsx
@@ -1,10 +1,12 @@
 import ChordComponent from "./components/ChordComponent";
+import PianoComponent from "./components/PianoComponent";
 
 export function App() {
   return (
-    <header className="h-screen bg-gradient-to-b from-gray-900 to-slate-800 flex flex-col text-white p-20">
+    <header className="h-screen bg-gradient-to-b from-gray-900 to-slate-800 flex-col text-white p-20">
       <h1 className="text-5xl font-bold text-center mb-2">Chord Trainer</h1>
       <ChordComponent />
+      <PianoComponent />
     </header>
   );
 }
---
+++ src/components/PianoComponent.tsx
@@ -0,0 +1,32 @@
+const PianoComponent = () => {
+  return (
+    <ul className="keyboard flex flex-row">
+      <li className="c-key h-36 w-8 bg-white z-0 border-r-2"></li>
+      <li className="cs-key h-24 w-6 bg-black z-10 border-r-2 -ml-3 -mr-3"></li>
+      <li className="d-key h-36 w-8 bg-white z-0 border-r-2"></li>
+      <li className="ds-key h-24 w-6 bg-black z-10 border-r-2 -ml-3 -mr-3"></li>
+      <li className="e-key h-36 w-8 bg-white z-0 border-r-2"></li>
+      <li className="f-key h-36 w-8 bg-white z-0 border-r-2"></li>
+      <li className="fs-key h-24 w-6 bg-black z-10 border-r-2 -ml-3 -mr-3"></li>
+      <li className="g-key h-36 w-8 bg-white z-0 border-r-2"></li>
+      <li className="gs-key h-24 w-6 bg-black z-10 border-r-2 -ml-3 -mr-3"></li>
+      <li className="a-key h-36 w-8 bg-white z-0 border-r-2"></li>
+      <li className="as-key h-24 w-6 bg-black z-10 border-r-2 -ml-3 -mr-3"></li>
+      <li className="b-key h-36 w-8 bg-white z-0 border-r-2"></li>
+      <li className="c2-key h-36 w-8 bg-white z-0 border-r-2"></li>
+      <li className="cs2-key h-24 w-6 bg-black z-10 border-r-2 -ml-3 -mr-3"></li>
+      <li className="d2-key h-36 w-8 bg-white z-0 border-r-2"></li>
+      <li className="ds2-key h-24 w-6 bg-black z-10 border-r-2 -ml-3 -mr-3"></li>
+      <li className="e2-key h-36 w-8 bg-white z-0 border-r-2"></li>
+      <li className="f2-key h-36 w-8 bg-white z-0 border-r-2"></li>
+      <li className="fs2-key h-24 w-6 bg-black z-10 border-r-2 -ml-3 -mr-3"></li>
+      <li className="g2-key h-36 w-8 bg-white z-0 border-r-2"></li>
+      <li className="gs2-key h-24 w-6 bg-black z-10 border-r-2 -ml-3 -mr-3"></li>
+      <li className="a2-key h-36 w-8 bg-white z-0 border-r-2"></li>
+      <li className="as2-key h-24 w-6 bg-black z-10 border-r-2 -ml-3 -mr-3"></li>
+      <li className="b2-key h-36 w-8 bg-white z-0 border-r-2"></li>
+    </ul>
+  );
+};
+
+export default PianoComponent;
